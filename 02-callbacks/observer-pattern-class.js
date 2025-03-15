7401b6e848e172b5bc6c0de7cc7e7368136dd5de
Axel Espinosa <axeldavid45@gmail.com>
2025-03-15 13:23:42 -0600

    observer pattern

---
+++ 02-callbacks/observer-pattern-class.js
@@ -0,0 +1,46 @@
+import { EventEmitter } from "node:events";
+import { readFile } from "node:fs";
+import { join } from "node:path";
+
+class FindRegex extends EventEmitter {
+  constructor(regex) {
+    super();
+    this.regex = regex;
+    this.files = [];
+  }
+
+  addFile(file) {
+    this.files.push(file);
+    return this;
+  }
+
+  find() {
+    for (const file of this.files) {
+      readFile(file, "utf-8", (err, content) => {
+        if (err) {
+          return this.emit("error", err);
+        }
+
+        this.emit("fileread", file);
+
+        const match = content.match(this.regex);
+        if (match) {
+          match.forEach((elem) => this.emit("found", file, elem));
+        }
+      });
+    }
+    return this;
+  }
+}
+
+
+const findFileRegex = new FindRegex(/hello \w+/g);
+
+findFileRegex
+  .addFile(join(import.meta.dirname, "data", "hello-world.txt"))
+  .addFile(join(import.meta.dirname, "data", "example.json"))
+  .find()
+  .on("fileread", (file) => console.log(`${file} was read`))
+  .on("found", (file, match) => console.log(`Matched "${match}" in ${file}`))
+  .on("error", (err) => console.error(`Error emitted ${err.message}`));
+  
