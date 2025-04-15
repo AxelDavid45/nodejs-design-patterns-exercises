0eb78c5ce24812640cc03ab8635d12004156ad72
Axel Espinosa <axeldavid45@gmail.com>
2025-04-14 20:18:30 -0600

    first version spider-cli: introduce callbackhell

--- 02-callbacks/solutions-exercises/ticker-exercise.js
+++ 02-callbacks/solutions-exercises/ticker-exercise.js
@@ -1,12 +1,20 @@
 import { EventEmitter } from "node:events";
 
-// This was my solution but i think it's not the best one
+//3.3 This was my solution but i think it's not the best one
 // because i dont use setTimeout and the book says to use it
 // even though this works
+
+// 3.4 modify the function created in exercise 3-3 so that
+// it produces an error if the timestamp at the moment of a tick is divisible by 5
 function Ticker(number, callback) {
   const emitter = new EventEmitter();
 
   process.nextTick(() => {
+    const timestamp = Date.now();
+    if (timestamp % 5 === 0) {
+      return emitter.emit("error", new Error("Timestamp is divisible by 5"));
+    }
+
     emitter.emit("tick", "starts");
   });
 
@@ -15,6 +23,15 @@ function Ticker(number, callback) {
   const idInterval = setInterval(() => {
     if (ticksEmitted < number) {
       ticksEmitted++;
+      const timestamp = Date.now();
+      const divisible = timestamp % 5 === 0;
+
+      if (divisible) {
+        const error = new Error("Timestamp is divisible by 5");
+        callback(error);
+        return emitter.emit("error", error);
+      }
+
       emitter.emit("tick");
     } else {
       clearInterval(idInterval);
@@ -29,6 +46,10 @@ const TIME = 10;
 
 Ticker(TIME, (ticksEmitted) => {
   console.log(`Emitted ${ticksEmitted} ticks`);
-}).on("tick", () => {
-  console.log("Tick");
-});
+})
+  .on("tick", () => {
+    console.log("Tick");
+  })
+  .on("error", (err) => {
+    console.error(`Error emitted ${err.message}`);
+  });
